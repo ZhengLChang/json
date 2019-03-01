e6136dd6bb4cabfb665eda8d55cd3f7b3fa4084e
zheng <zhenghuijie@atcom.com.cn>
2019-03-01 15:51:16 +0800

    增加form获取指接口

--- unescapeChars.c
+++ unescapeChars.c
@@ -2,24 +2,24 @@
 #include <string.h>
 #include <stdlib.h>
 
-static int cgiHexValue[256];
-static void cgiSetupConstants();
+static int HexValue[256];
+static void SetupConstants();
 typedef enum {
-  cgiEscapeRest,
-  cgiEscapeFirst,
-  cgiEscapeSecond
-} cgiEscapeState;
+  EscapeRest,
+  EscapeFirst,
+  EscapeSecond
+} EscapeState;
 
 typedef enum {
-  cgiUnescapeSuccess,
-  cgiUnescapeMemory
-} cgiUnescapeResultType;
+  UnescapeSuccess,
+  UnescapeMemory
+} UnescapeResultType;
 typedef enum {
-  cgiParseSuccess,
-  cgiParseMemory,
-  cgiParseIO
-} cgiParseResultType;
-typedef struct cgiFormEntryStruct {
+  ParseSuccess,
+  ParseMemory,
+  ParseIO
+} ParseResultType;
+typedef struct FormEntryStruct {
   char *attr;
   /* value is populated for regular form fields only.
    * *    For file uploads, it points to an empty string, and file
@@ -34,103 +34,107 @@ typedef struct cgiFormEntryStruct {
   char *contentType;
   /* Temporary file name for working storage of file uploads. */
   char *tfileName;
-  struct cgiFormEntryStruct *next;
-} cgiFormEntry;
-static cgiFormEntry *cgiFormEntryFirst;
-cgiUnescapeResultType cgiUnescapeChars(char **sp, char *cp, int len);
-static cgiParseResultType cgiParseFormInput(char *data, int length);
-static void cgiFreeResources();
+  struct FormEntryStruct *next;
+} FormEntry;
+static FormEntry *FormEntryFirst;
+UnescapeResultType UnescapeChars(char **sp, char *cp, int len);
+static ParseResultType ParseFormInput(char *data, int length);
+static void FreeResources();
+int cgigetinteger_simple(char *name, const int def);
+const char *cgigetstring_simple(char *name, const char *def);
 int main(void){ 
   char *sp = NULL;
   char cp[] = "PnPEnable=1&DHCPEnable=1&dhcpCustomOptionFor66=&provisioningServer=&userName=%26&password=&commonAESKey=&MACAESKey=&checkNewConfig=1&repeatedly=0&interval=1440&weekly=0&startHour=3&startMinute=0&stopHour=3&stopMinute=0&dayOfWeekly=1%2C1%2C0%2C0%2C0%2C0%2C1&sunday=1&monday=1&tuesday=0&wednesday=0&thursday=0&friday=0&saturday=1&user_set_auto";
-  cgiSetupConstants();
-  cgiParseFormInput(cp, strlen(cp));
-  if(cgiFormEntryFirst != NULL){
-    struct cgiFormEntryStruct *p = NULL;
-    for(p = cgiFormEntryFirst; p ; p = p->next){
+  SetupConstants();
+  ParseFormInput(cp, strlen(cp));
+  if(FormEntryFirst != NULL){
+    struct FormEntryStruct *p = NULL;
+    for(p = FormEntryFirst; p ; p = p->next){
       printf("%s = %s\n", p->attr, p->value);
     }
   }
-
-  cgiFreeResources();
-//  cgiUnescapeChars(&sp, cp, strlen(cp));
+  printf("DHCPEnable = %d\n", cgigetinteger_simple("DHCPEnable", 0));
+  printf("userName = %s\n", cgigetstring_simple("userName", ""));
+  printf("dayOfWeekly = %s\n", cgigetstring_simple("dayOfWeekly", ""));
+  FreeResources();
+//  UnescapeChars(&sp, cp, strlen(cp));
 //  printf("%s\n", sp);
   return 0;
 }
 
-cgiUnescapeResultType cgiUnescapeChars(char **sp, char *cp, int len) {
+UnescapeResultType UnescapeChars(char **sp, char *cp, int len) {
   char *s;
-  cgiEscapeState escapeState = cgiEscapeRest;
+  EscapeState escapeState = EscapeRest;
   int escapedValue = 0;
   int srcPos = 0;
   int dstPos = 0;
   s = (char *) malloc(len + 1);
   if (!s) {
-    return cgiUnescapeMemory;
+    return UnescapeMemory;
   }
   while (srcPos < len) {
     int ch = cp[srcPos];
     switch (escapeState) {
-      case cgiEscapeRest:
+      case EscapeRest:
         if (ch == '%') {
-          escapeState = cgiEscapeFirst;
+          escapeState = EscapeFirst;
         } else if (ch == '+') {
           s[dstPos++] = ' ';
         } else {
           s[dstPos++] = ch; 
         }
         break;
-      case cgiEscapeFirst:
-        escapedValue = cgiHexValue[ch] << 4;  
-        escapeState = cgiEscapeSecond;
+      case EscapeFirst:
+        escapedValue = HexValue[ch] << 4;  
+        escapeState = EscapeSecond;
         break;
-      case cgiEscapeSecond:
-        escapedValue += cgiHexValue[ch];
+      case EscapeSecond:
+        escapedValue += HexValue[ch];
         s[dstPos++] = escapedValue;
-        escapeState = cgiEscapeRest;
+        escapeState = EscapeRest;
         break;
     }
     srcPos++;
   }
   s[dstPos] = '\0';
   *sp = s;
-  return cgiUnescapeSuccess;
+  return UnescapeSuccess;
 }
-static void cgiSetupConstants() {
+static void SetupConstants() {
   int i;
   for (i=0; (i < 256); i++) {
-    cgiHexValue[i] = 0;
+    HexValue[i] = 0;
   }
-  cgiHexValue['0'] = 0; 
-  cgiHexValue['1'] = 1; 
-  cgiHexValue['2'] = 2; 
-  cgiHexValue['3'] = 3; 
-  cgiHexValue['4'] = 4; 
-  cgiHexValue['5'] = 5; 
-  cgiHexValue['6'] = 6; 
-  cgiHexValue['7'] = 7; 
-  cgiHexValue['8'] = 8; 
-  cgiHexValue['9'] = 9;
-  cgiHexValue['A'] = 10;
-  cgiHexValue['B'] = 11;
-  cgiHexValue['C'] = 12;
-  cgiHexValue['D'] = 13;
-  cgiHexValue['E'] = 14;
-  cgiHexValue['F'] = 15;
-  cgiHexValue['a'] = 10;
-  cgiHexValue['b'] = 11;
-  cgiHexValue['c'] = 12;
-  cgiHexValue['d'] = 13;
-  cgiHexValue['e'] = 14;
-  cgiHexValue['f'] = 15;
+  HexValue['0'] = 0; 
+  HexValue['1'] = 1; 
+  HexValue['2'] = 2; 
+  HexValue['3'] = 3; 
+  HexValue['4'] = 4; 
+  HexValue['5'] = 5; 
+  HexValue['6'] = 6; 
+  HexValue['7'] = 7; 
+  HexValue['8'] = 8; 
+  HexValue['9'] = 9;
+  HexValue['A'] = 10;
+  HexValue['B'] = 11;
+  HexValue['C'] = 12;
+  HexValue['D'] = 13;
+  HexValue['E'] = 14;
+  HexValue['F'] = 15;
+  HexValue['a'] = 10;
+  HexValue['b'] = 11;
+  HexValue['c'] = 12;
+  HexValue['d'] = 13;
+  HexValue['e'] = 14;
+  HexValue['f'] = 15;
   return;
 }
 
-static cgiParseResultType cgiParseFormInput(char *data, int length) {
+static ParseResultType ParseFormInput(char *data, int length) {
 	/* Scan for pairs, unescaping and storing them as they are found. */
 	int pos = 0;
-	cgiFormEntry *n;
-	cgiFormEntry *l = 0;
+	FormEntry *n;
+	FormEntry *l = 0;
 	while (pos != length) {
 		int foundEq = 0;
 		int foundAmp = 0;
@@ -150,9 +154,9 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 		if (!foundEq) {
 			break;
 		}
-		if (cgiUnescapeChars(&attr, data+start, len)
-			!= cgiUnescapeSuccess) {
-			return cgiParseMemory;
+		if (UnescapeChars(&attr, data+start, len)
+			!= UnescapeSuccess) {
+			return ParseMemory;
 		}	
 		start = pos;
 		len = 0;
@@ -167,17 +171,17 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 		}
 		/* The last pair probably won't be followed by a &, but
 			that's fine, so check for that after accepting it */
-		if (cgiUnescapeChars(&value, data+start, len)
-			!= cgiUnescapeSuccess) {
+		if (UnescapeChars(&value, data+start, len)
+			!= UnescapeSuccess) {
 			free(attr);
-			return cgiParseMemory;
+			return ParseMemory;
 		}	
 		/* OK, we have a new pair, add it to the list. */
-		n = (cgiFormEntry *) malloc(sizeof(cgiFormEntry));	
+		n = (FormEntry *) malloc(sizeof(FormEntry));
 		if (!n) {
 			free(attr);
 			free(value);
-			return cgiParseMemory;
+			return ParseMemory;
 		}
 		n->attr = attr;
 		n->value = value;
@@ -187,7 +191,7 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 			free(attr);
 			free(value);
 			free(n);
-			return cgiParseMemory;
+			return ParseMemory;
 		}	
 		n->fileName[0] = '\0';
 		n->contentType = (char *) malloc(1);
@@ -196,7 +200,7 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 			free(value);
 			free(n->fileName);
 			free(n);
-			return cgiParseMemory;
+			return ParseMemory;
 		}	
 		n->contentType[0] = '\0';
 		n->tfileName = (char *) malloc(1);
@@ -206,12 +210,12 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 			free(n->fileName);
 			free(n->contentType);
 			free(n);
-			return cgiParseMemory;
+			return ParseMemory;
 		}	
 		n->tfileName[0] = '\0';
 		n->next = 0;
 		if (!l) {
-			cgiFormEntryFirst = n;
+			FormEntryFirst = n;
 		} else {
 			l->next = n;
 		}
@@ -220,12 +224,12 @@ static cgiParseResultType cgiParseFormInput(char *data, int length) {
 			break;
 		}			
 	}
-	return cgiParseSuccess;
+	return ParseSuccess;
 }
 
-static void cgiFreeResources() {
-    cgiFormEntry *c = cgiFormEntryFirst;
-    cgiFormEntry *n;
+static void FreeResources() {
+    FormEntry *c = FormEntryFirst;
+    FormEntry *n;
     while (c) {
       n = c->next;
       free(c->attr);
@@ -239,6 +243,24 @@ static void cgiFreeResources() {
       free(c);
       c = n;
     }
-    cgiFormEntryFirst = NULL;
+    FormEntryFirst = NULL;
     return;
+}
+int cgigetinteger_simple(char *name, const int def){
+	FormEntry *next = NULL;
+	for(next = FormEntryFirst; next; next = next->next){
+		if(strcmp(next->attr, name) == 0){
+			return atoi(next->value);
+		}
+	}
+    return def;
+}
+const char *cgigetstring_simple(char *name, const char *def){
+	FormEntry *next = NULL;
+	for(next = FormEntryFirst; next; next = next->next){
+		if(strcmp(next->attr, name) == 0){
+			return next->value;
+		}
+	}
+    return def;
 }
